3038c34eabde0e9fb03a88a0bee0c65bbf1b39ed
AuControl <aucontrolac@gmail.com>
2026-01-25 18:51:51 -0500

    overview image added

--- InvertekDrive-RTU-UI/InvertekDrive-RTU-UI/obj/Debug/net10.0-windows/App.g.cs
+++ InvertekDrive-RTU-UI/InvertekDrive-RTU-UI/obj/Debug/net10.0-windows/App.g.cs
@@ -58,7 +58,7 @@ namespace InvertekDrive_RTU_UI {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/InvertekDrive-RTU-UI;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/InvertekDrive-RTU-UI;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
